a8ca7af3c36bb1740722f734c4f25e4a7de0389c
Vishnu Sivan <codemaker2015@gmail.com>
2022-01-12 19:39:51 +0530

    Updated with Android app demo

--- README.md
+++ README.md
@@ -38,8 +38,8 @@ You can use an explicit Android intent to launch Scene Viewer. It can be trigger
 ```
 
 ### Screenshots
-
-![Screenshot1](https://github.com/codemaker2015/scene-viewer-demo/blob/master/resources/screenshot1.jpeg)
-![Screenshot2](https://github.com/codemaker2015/scene-viewer-demo/blob/master/resources/screenshot2.jpeg)
-![Screenshot3](https://github.com/codemaker2015/scene-viewer-demo/blob/master/resources/screenshot3.jpeg)
+<img src="resources/demo.gif" width="270" height="585 />
+![Screenshot1](resources/screenshot1.jpeg)
+![Screenshot2](resources/screenshot2.jpeg)
+![Screenshot3](resources/screenshot3.jpeg)
 
---
+++ android/app/src/main/java/com/example/sceneviewerdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sceneviewerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnView = findViewById(R.id.btnView);
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+                sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"));
+                sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+                startActivity(sceneViewerIntent);
+            }
+        });
+    }
+}
